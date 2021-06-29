f02007afcfdd42e9b4c676931e04be4509f66a75
kunallverma007 <58910303+kunallverma007@users.noreply.github.com>
2021-06-29 18:49:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app=express();
-const cors=require('cors')
+// const cors=require('cors')
 const authRoute = require('./routes/auth');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -12,7 +12,7 @@ mongoose.connect(process.env.DB_CONNECT,{
  },()=>console.log('connected to db'));
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
 app.use('/',authRoute);
 app.listen(process.env.PORT || 3001,function(){
     
